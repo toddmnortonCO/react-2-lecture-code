20413e6436102f103145cb843ef8bde739375c74
mattcbodily <mattcbodily@gmail.com>
2020-09-17 09:34:08 -0600

    lecture boiler-plate complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Header from './Components/Header';
+import ListDisplay from './Components/ListDisplay';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      username: 'Matt',
+      todoList: ['Code the things', 'Eat', 'Sleep']
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Header />
+        <h1>My Todo List</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
